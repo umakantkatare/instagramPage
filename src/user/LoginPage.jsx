3e291b95b052b096c069abdc243c25cd0b7cfbbe
Umakant Katare <umakantkatare9753121329@gmail.com>
2024-01-27 22:25:07 +0530

    add feature

--- src/user/LoginPage.jsx
+++ src/user/LoginPage.jsx
@@ -1,15 +1,39 @@
 /* eslint-disable no-unreachable */
 /* eslint-disable no-unused-vars */
-import React from "react";
-import logo from "./assets/instagram.png";
+import React, { useState } from "react";
+import logo from "../assets/instagram.png";
 
 function LoginPage() {
+  const [login, setLogin] = useState(true);
+
+  const logInSwitch = () => {
+    setLogin(!login);
+  };
   return (
     <div className=" min-h-screen bg-slate-500 flex items-center justify-center flex-col gap-3 ">
-      <div className="bg-black  p-4 rounded-md  w-96">
+      <div className="bg-black text-white  p-4 rounded-md  w-96">
         <h1 className=" text-2xl font-bold text-center text-white mb-4 ">
           <img src={logo} alt="" />
         </h1>
+        <div className=" gap-2">
+          <input
+            hidden={login}
+            type="text"
+            placeholder="email or mobile number"
+          />
+          <input hidden={login} type="text" placeholder="fullname" />
+          <input
+            className=" "
+            placeholder="username, phone number / email"
+            type="text"
+          />
+          <input placeholder="password" type="text" />
+          <button>login</button>
+          <div>
+            {login ? "Don't have an account" : "Have an account"}
+            <span onClick={logInSwitch}>{login ? "Sign Up" : "Log In"}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
